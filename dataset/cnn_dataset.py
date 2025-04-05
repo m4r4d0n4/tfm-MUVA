0c2ea6cbc1f03b359f69fd29551d38710f9fcc6c
m4r4d0n4 <juansebasmontes@gmail.com>
2025-04-05 21:24:11 +0200

    fix: remove extra if statement in cnn_dataset.py

--- dataset/cnn_dataset.py
+++ dataset/cnn_dataset.py
@@ -6,7 +6,7 @@ from torchvision import transforms
 class WikiArtCNNDataset(Dataset):
     def __init__(self, split="train", validation_split=0.2):
         # Load the entire dataset
-        self.ds = load_dataset("huggan/wikiart", split="train")
+        ds = load_dataset("huggan/wikiart", split="train")
 
         # Split into training and validation sets
         split_ds = self.ds.train_test_split(test_size=validation_split, seed=42)
@@ -17,19 +17,26 @@ class WikiArtCNNDataset(Dataset):
         self.artist_to_index = {artist: i for i, artist in enumerate(self.artists)}
 
         if split == "train":
+            transform_list = [
+                transforms.Resize((224, 224)),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(degrees=20),
+                transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+            self.transform = transforms.Compose(transform_list)
             self.ds = train_ds
         elif split == "validation":
-            self.ds = val_ds
-        else:
-            raise ValueError("Invalid split. Must be 'train' or 'validation'")
-
-        self.transform = transforms.Compose(
-            [
+            transform_list = [
                 transforms.Resize((224, 224)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
-        )
+            self.transform = transforms.Compose(transform_list)
+            self.ds = val_ds
+        else:
+            raise ValueError("Invalid split. Must be 'train' or 'validation'")
 
     def __len__(self):
         return len(self.ds)
