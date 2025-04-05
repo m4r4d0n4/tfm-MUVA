9e55ca9967331a7bbe3f9247508862b7f71eb9c8
m4r4d0n4 <juansebasmontes@gmail.com>
2025-04-05 19:44:44 +0200

    fix: more running workers

--- scripts/train_vit.py
+++ scripts/train_vit.py
@@ -18,8 +18,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Load the datasets
 train_dataset = WikiArtCNNDataset(split="train")
 val_dataset = WikiArtCNNDataset(split="validation")
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
-val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=10)
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=10)
 
 # Load the model
 num_classes = train_dataset.get_num_classes()
